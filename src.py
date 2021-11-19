2fe480ec167a218b327f37479792355408e2d486
Stefany Izidio <stefanyvitoria9307@gmail.com>
2021-11-19 20:14:09 -0300

    teste

--- src.py
+++ src.py
@@ -1,3 +1,23 @@
-cafezinhoooooooo
+class Node:
+  def __init__(self, label):
+    self.label = label
+    self.nodesAdj = []
+  
 
-Testando123456
+
+class Graph:
+  def __init__(self, nodes=[]):
+      self.nodes = nodes
+
+  def addNode(self, label):
+    self.nodes.append(Node(label=label,))
+    return
+  
+  def deleteNode(self):
+    pass
+
+  def addEdge(self):
+    pass
+
+  def deleteEdge(self):
+    pass
